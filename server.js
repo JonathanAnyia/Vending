f1b5cb1e03f85474d801270cfe901d0f5ba18655
Jonathan Anyia <jonathananyia7@gmail.com>
2025-11-11 07:10:47 -0800

    server again

--- server.js
+++ server.js
@@ -8,7 +8,20 @@ dotenv.config();
 
 
 const app = express();
-app.use(cors({origin: '*'}));
+
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+app.use(cors({origin: ['*', 'http://localhost:5174', 'http://localhost:5173']}));
 app.use(express.json());
 
 
