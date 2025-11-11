5e51096f6c6e5a0dfeef6318d3f10a00c35cce99
Jonathan Anyia <jonathananyia7@gmail.com>
2025-11-11 08:41:42 -0800

    prefixes

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ app.use(cors({
   origin: ['http://localhost:5173', 'http://localhost:5174'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
 }));
 
 app.use(express.json());
